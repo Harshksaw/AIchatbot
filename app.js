3f602af00c7228d191d41faab729edcee5d5c672
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2023-09-24 19:45:13 +0530

    CORS added

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const app = express();
 const authRoute = require('./router/authRoute');
 const databaseconnect = require('./config/databaseConnect');
 const cookieParser = require('cookie-parser');
-
+const cors = require('cors')
 databaseconnect();
 
 app.use(express.json());
 app.use(cookieParser());   //Parse before going to any route
-
+app.use(cors ({
+    origin: [process.env.CLIENT_URL]
+    ,credentials: true
+}))
 
 app.use('/api/auth/',  authRoute);
 

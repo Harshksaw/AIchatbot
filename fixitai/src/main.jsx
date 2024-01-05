0200187c23c30bdd0662a29a7fee19a7e859ec68
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2024-01-05 13:09:45 +0530

    FIxitapi|| front end

--- fixitai/src/main.jsx
+++ fixitai/src/main.jsx
@@ -6,16 +6,16 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Auth0Provider
-    domain="dev-2qx41ykchnb0sevp.us.auth0.com"
-    clientId="ezqWhX9Ia9DPpHt88dNLCjw5xRZgN8MF"
-    authorizationParams={{
-      redirect_uri: 'https://backend-login-signup.vercel.app/api/auth/callback' // Use the allowed URL from Auth0
-    }}
-  >
-    <App />
-  </Auth0Provider>
-);
+  domain="dev-2qx41ykchnb0sevp.us.auth0.com"
+  clientId="ezqWhX9Ia9DPpHt88dNLCjw5xRZgN8MF"
+  authorizationParams={{
+    redirect_uri: window.location.origin
+  }}
+>
+  <App />
+</Auth0Provider>,
 
-
+)
